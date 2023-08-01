e3f426b7057573fd7157f13b06fa4929d6b1124e
Brooke Adlesperger-Jones <badles@live.com>
2023-07-31 22:33:05 -0500

    added formatDate.js file

--- models/Reaction.js
+++ models/Reaction.js
@@ -30,6 +30,4 @@ const reactionSchema = new Schema(
     }
 );
 
-const Reaction = model('Reaction', reactionSchema);
-
 module.exports = Reaction;
---
+++ utils/formatDate.js
@@ -0,0 +1,5 @@
+function dateFormat(date) {
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+}   
+
+module.exports = dateFormat;
