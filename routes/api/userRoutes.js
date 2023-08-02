7f5e4fc6592d30c19c57cc9edf1cd03303822944
Brooke Adlesperger-Jones <badles@live.com>
2023-08-02 17:10:43 -0500

    updated user routes file

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -9,3 +9,25 @@ const {
     deleteFriend,
 } = require('../../controllers/userController');
 
+// /api/users
+router
+    .route('/')
+    .get(getUsers)
+    .post(createUser);
+
+// /api/users/:id
+router
+    .route('/:id')
+    .get(getSingeUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// /api/users/:userId/friends/:friendId
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
+
+
