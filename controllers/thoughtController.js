5eaef1ad4afb2b973f0d63f7ed9b4c23f76ea8ba
Brooke Adlesperger-Jones <badles@live.com>
2023-08-02 17:42:16 -0500

    updated get thoughts function

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,20 +1,15 @@
 const { Thought, User } = require('../models');
 
-const thoughtController = {
+module.exports = {
     // get all thoughts
-    getAllThoughts(req, res) {
-        Thought.find({})
-            .populate({
-                path: 'reactions',
-                select: '-__v'
-            })
-            .select('-__v')
-            .sort({ _id: -1 })
-            .then(dbThoughtData => res.json(dbThoughtData))
-            .catch(err => {
-                res.sendStatus(400);
-            }
-            );
+    async getThoughts(req, res) {
+        try {
+            const thoughts = await Thought.find({})
+               
+            res.json(thoughts);
+        } catch (err) {
+            res.sendStatus(400);
+        }
     },
 
     // get one thought by id
