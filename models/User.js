cda0bbaf8f569a8b29fb5454f5e49ac7b7b532e5
Brooke Adlesperger-Jones <badles@live.com>
2023-08-02 17:32:47 -0500

    updated date format file name

--- models/User.js
+++ models/User.js
@@ -12,7 +12,7 @@ const userSchema = new Schema(
       type: String,
       required: true,
       unique: true,
-      match: [/.+@.+\..+{2}/, "Is not a valid email address"],
+      match: [/.+@.+\..+/],
     },
     thoughts: [
       {
